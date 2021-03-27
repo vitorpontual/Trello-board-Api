e6a99abcf05c9c224981c159d1281ecddfc3f01c
Vitor Pontual <vitorpguedes@me.com>
2021-03-27 14:11:39 -0300

    settings timezone

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,55 @@ dotenv.config()
 
 const url = `https://api.trello.com/1/cards?key=${process.env.REACT_APP_TRELLO_KEY}&token=${process.env.REACT_APP_TRELLO_TOKEN}&idList=${process.env.REACT_APP_TRELLO_ID_LIST}`
 
-const initialValue = {
-  name: "",
-  email: "",
-  desc: "",
-  idLables: "",
-  due: "",
-}
+
+
 
 function App() {
+  const dt = Date.now()
+  const initialValue = {
+    name: "",
+    email: "",
+    desc: "",
+    idLabels: "",
+    due: date(dt).oneHour,
+  }
   const [values, setValues] = useState(initialValue)
+ 
 
   function onChange(event: any) {
-    const { name, value } = event.target
-
+    const { name, value} = event.target
+   
     setValues({ ...values, [name]: value })
+    console.log(values)
 
   }
 
+  function date(timestamp: any){
+    const date = new Date(timestamp)
 
-  const myDate = new Date();
+    const oneHour = date.setHours(date.getHours()  + 1)
+    const threeHour = date.setHours(date.getHours()  + 2)
+    const sixHour = date.setHours(date.getHours()  + 3)
+
+
+    return {
+      oneHour,
+      threeHour,
+      sixHour,
+    }
+  }
 
   const options = [
-    { value: myDate.setHours(myDate.getHours() + 2), label: "2 Hour" },
-    { value: myDate.setHours(myDate.getHours() + 4), label: "3 Hours" },
-    { value: myDate.setHours(myDate.getHours() + 6), label: "5 hours" },
+    { value: date(dt).oneHour},
+    { value: date(dt).threeHour},
+    { value: date(dt).sixHour}
   ]
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault()
+   
     const jason = JSON.stringify(values)
-    console.log(jason)
+    
 
 
     await fetch(url, {
@@ -58,7 +76,10 @@ function App() {
       .then(text => console.log(text))
       .catch(err => console.error(err));
 
-    window.location.reload()
+      alert("Form Sended")
+
+      window.location.reload();
+
   }
 
   return (
@@ -97,7 +118,7 @@ function App() {
             <div className="item checkbox">
               <label className="checkbox">
                 <span className="checkbox__input">
-                  <input type="checkbox" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3ac" />
+                  <input type="radio" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3ac" />
                   <span className="checkbox__control">
                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-hidden="true" focusable="false">
                       <path fill='none' stroke='currentColor' stroke-width='3' d='M1.73 12.91l6.37 6.37L22.79 4.59' /></svg>
@@ -107,7 +128,7 @@ function App() {
               </label>
               <label className="checkbox">
                 <span className="checkbox__input">
-                  <input type="checkbox" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3b3" />
+                  <input type="radio" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3b3" />
                   <span className="checkbox__control">
                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-hidden="true" focusable="false">
                       <path fill='none' stroke='currentColor' stroke-width='3' d='M1.73 12.91l6.37 6.37L22.79 4.59' /></svg>
@@ -117,7 +138,7 @@ function App() {
               </label>
               <label className="checkbox">
                 <span className="checkbox__input">
-                  <input type="checkbox" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3b6" />
+                  <input type="radio" name="idLabels" onChange={onChange} value="605e18e3184d2c731b95b3b6" />
                   <span className="checkbox__control">
                     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-hidden="true" focusable="false">
                       <path fill='none' stroke='currentColor' stroke-width='3' d='M1.73 12.91l6.37 6.37L22.79 4.59' /></svg>
@@ -131,14 +152,15 @@ function App() {
             <div className="item">
               <label htmlFor="due"></label>
               <select className="due" name="due" onChange={onChange}>
-                  <option value={options[0].value}>+ 1 Hour</option>
+                  <option value={options[0].value}>+1 Hour</option>
                   <option value={options[1].value}>+3 Hours</option>
-                  <option value={options[2].value}>+5 Hours</option>
+                  <option value={options[2].value}>+6 Hours</option>
               </select>
 
             </div>
             <div className="item">
-              <div className="tags"></div>
+              <div className="tags">
+              </div>
             </div>
             <button className="confirm-button" type="submit">Send</button>
           </div>
